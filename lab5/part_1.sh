282242460823392cb2b03ded8533b731f1786cd6
Maya <degafemaya@gmail.com>
2025-03-23 07:19:35 +0000

    added more edits to part 1

--- lab5/part_1.sh
+++ lab5/part_1.sh
@@ -15,8 +15,19 @@ jq '.[].receiptTime' aviation.json | head -n 6
 #5. Parses the data and pulls out each of the tempurature
 #6. OUTPUTS the average tempurature across the 12 hours
 avgTemp=$(jq '[.[].temp] | add / length' aviation.json)
-echo "The average tempurature across the 12 hours: $average_temp"
+echo "Average Tempurature: $avgTemp"
 
 #7. Parses the data and pulls out each of the tempurature
 #8. OUTPUTS a boolean response for if more than half of the last 12 hours were cloudy (i.e. not CLR)
 
+cloudy_count=$(jq '[.[] | select(.clouds[].cover != "CLR")] | length' aviation.json)
+total_reports=$(jq 'length' aviation.json)
+
+
+cloudy=false
+
+if [ "$cloudy_count" -gt "$((total_reports/2))" ]; then
+	cloudy=true
+fi
+
+echo "Mostly Cloudy: $cloudy"
